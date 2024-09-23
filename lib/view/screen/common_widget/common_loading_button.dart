740fdd719c49264c16572d8ad19ade5c49027426
NS NooR <non377938@gmail.com>
2024-09-24 02:30:28 +0600

    common loading button

---
+++ lib/view/screen/common_widget/common_loading_button.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class CommonLoadingButton extends StatelessWidget {
+  const CommonLoadingButton({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    double screenHeight = MediaQuery.sizeOf(context).height;
+    double screenWidth = MediaQuery.sizeOf(context).width;
+    return Container(
+        height: screenHeight * 0.06,
+        width: screenWidth * 0.9,
+        decoration: BoxDecoration(
+            color: Colors.blue,
+            borderRadius: BorderRadius.circular(10)),
+        child: const Center(
+            child: CircularProgressIndicator(
+              color: Colors.white,
+              backgroundColor: Colors.blue,
+            )));
+  }
+}
