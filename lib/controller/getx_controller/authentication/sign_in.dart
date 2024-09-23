2fbcae599d9204f2c41d320cb0b1c2358c1914b4
NS NooR <non377938@gmail.com>
2024-09-24 02:31:30 +0600

    sign in

---
+++ lib/controller/getx_controller/authentication/sign_in.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/cupertino.dart';
+import 'package:get/get.dart';
+
+class SignInController extends GetxController {
+  RxBool isLoading = false.obs;
+  TextEditingController mailController = TextEditingController();
+  TextEditingController passController = TextEditingController();
+
+  // Future<bool> signInService() async {
+  //   bool status = await SignInService.signInService(
+  //       mail: mailController.text, pass: passController.text);
+  //   return status;
+  // }
+
+  @override
+  void onInit() {
+    mailController.text = "noor@gmail.com";
+    passController.text = "12345678";
+    super.onInit();
+  }
+
+  @override
+  void dispose() {
+    mailController.dispose();
+    passController.dispose();
+    super.dispose();
+  }
+}
