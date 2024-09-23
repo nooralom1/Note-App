05a2676d2a2e40126781cbc6abe8dd6548ac8b04
NS NooR <non377938@gmail.com>
2024-09-24 02:31:40 +0600

    sign up

---
+++ lib/controller/getx_controller/authentication/sign_up.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/cupertino.dart';
+import 'package:get/get.dart';
+
+class SignUpController extends GetxController {
+  RxBool isLoading = false.obs;
+  TextEditingController nameController = TextEditingController();
+  TextEditingController mailController = TextEditingController();
+  TextEditingController passController = TextEditingController();
+
+  // Future<bool> signUpService() async {
+  //   bool status = await SignUpService.signUpService(
+  //       name: nameController.text,
+  //       mail: mailController.text,
+  //       pass: passController.text);
+  //   return status;
+  // }
+
+  @override
+  void onInit() {
+    nameController.text = "Noor Alam";
+    mailController.text = "noor@gmail.com";
+    passController.text = "12345678";
+    super.onInit();
+  }
+
+  @override
+  void dispose() {
+    mailController.dispose();
+    passController.dispose();
+    nameController.dispose();
+    super.dispose();
+  }
+}
