140a0d7a937dd22cba2c766c7254a592a0886c2e
NS NooR <non377938@gmail.com>
2024-09-25 23:45:11 +0600

    note app

--- lib/controller/firebase_controller/add_notes.dart
+++ lib/controller/firebase_controller/add_notes.dart
@@ -1,5 +1,3 @@
-
-import 'dart:developer';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/cupertino.dart';
 
@@ -7,16 +5,12 @@ class AddNotesService {
   static Future<bool> addNoteService(
       {required Map<String, dynamic> notes}) async {
     try {
-      log("+++++++++++++");
       FirebaseFirestore firebaseFirestore = FirebaseFirestore.instance;
       await firebaseFirestore.collection("Notes").add(notes);
-      log("++++++111111+++++++");
       return true;
     } catch (e) {
-      log("+++++++22222222222++++++");
       debugPrint("Error: $e");
     }
-    log("++++++333333+++++++");
     return false;
   }
 }
