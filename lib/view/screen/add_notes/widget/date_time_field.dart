bc6956d66fa598c659defe7ada220deb890e6fab
NS NooR <non377938@gmail.com>
2024-09-25 23:45:20 +0600

    note app

--- lib/view/screen/add_notes/widget/date_time_field.dart
+++ lib/view/screen/add_notes/widget/date_time_field.dart
@@ -3,8 +3,8 @@ import 'package:get/get.dart';
 
 class DateTimeField extends StatelessWidget {
   const DateTimeField({
-    super.key, required this.controller,
-
+    super.key,
+    required this.controller,
   });
 
   final TextEditingController controller;
@@ -12,12 +12,12 @@ class DateTimeField extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SizedBox(
-      height: Get.height*0.06,
-      width: Get.width*0.42,
+      height: Get.height * 0.06,
+      width: Get.width * 0.42,
       child: TextField(
         controller: controller,
-        decoration:  InputDecoration(
-            hintText: DateTime.now().toString().substring(0,16),
+        decoration: InputDecoration(
+            hintText: DateTime.now().toString().substring(0, 16),
             border: const OutlineInputBorder(borderSide: BorderSide.none)),
       ),
     );
