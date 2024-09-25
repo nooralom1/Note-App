3cec16b0bacf1c3c4fd2cc362c5e82fd1a195711
NS NooR <non377938@gmail.com>
2024-09-25 23:45:48 +0600

    note app

--- lib/view/screen/add_notes/widget/description_field.dart
+++ lib/view/screen/add_notes/widget/description_field.dart
@@ -3,7 +3,8 @@ import 'package:get/get.dart';
 
 class DescriptionField extends StatelessWidget {
   const DescriptionField({
-    super.key, required this.controller,
+    super.key,
+    required this.controller,
   });
   final TextEditingController controller;
 
@@ -13,8 +14,7 @@ class DescriptionField extends StatelessWidget {
       height: Get.height * 0.4,
       width: Get.width,
       decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(10),
-          border: Border.all()),
+          borderRadius: BorderRadius.circular(10), border: Border.all()),
       child: TextField(
         maxLines: 10,
         controller: controller,
