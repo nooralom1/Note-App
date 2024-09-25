f564ef1cbac83444d93fae31927c37943f6c53b3
NS NooR <non377938@gmail.com>
2024-09-25 23:45:26 +0600

    note app

--- lib/controller/firebase_controller/get_notes.dart
+++ lib/controller/firebase_controller/get_notes.dart
@@ -1,20 +1,16 @@
-import 'dart:developer';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/cupertino.dart';
 
 class GetNotesService {
   static Future<QuerySnapshot<Map<String, dynamic>>?> getNoteService() async {
     try {
-      log("+++++++++++++");
       FirebaseFirestore firebaseFirestore = FirebaseFirestore.instance;
       QuerySnapshot<Map<String, dynamic>> data =
       await firebaseFirestore.collection("Notes").get();
       return data;
     } catch (e) {
-      log("+++++++22222222222++++++");
       debugPrint("Error: $e");
     }
-    log("++++++333333+++++++");
     return null;
   }
 }
--- lib/controller/getx_controller/home.dart
+++ lib/controller/getx_controller/home.dart
@@ -1,25 +1,22 @@
 import 'dart:developer';
-
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:get/get.dart';
 import 'package:notes/controller/firebase_controller/get_notes.dart';
 
-class HomeController extends GetxController{
-
+class HomeController extends GetxController {
   RxBool isLoading = false.obs;
   final formKey = GlobalKey<FormState>();
   RxList<dynamic> user = [].obs;
 
   getNotes() async {
     isLoading.value = true;
-    log("================");
     QuerySnapshot<Map<String, dynamic>>? get =
-    await GetNotesService.getNoteService();
+        await GetNotesService.getNoteService();
     if (get != null) {
       for (var data in get.docs.toList()) {
         var name = ({data["tittle"]});
-        log("++++++++++++++user: $name");
+        log("$name");
       }
     }
     isLoading.value = false;
